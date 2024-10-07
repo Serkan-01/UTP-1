ec5d688ae30c8ed0c02d384021aade98c2b19903
Serkan-01 <s33309@pjwstk.edu.pl>
2024-10-07 18:34:04 +0200

    I will add `Adder` and s31880 will add `Substractor`.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 // TODO: we need to add the missing classes!
 
+// OK, I will add `Adder` and s31880 will add `Substractor`.
+
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
